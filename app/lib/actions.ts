67ff840be1386e752aca321d538940de0b51e5f3
Andrea Melero <ameleror@gmail.com>
2023-11-10 07:44:45 +0100

    added sql query to insert data of new invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
- import {z} from 'zod'
+ import {z} from 'zod';
+ import { sql } from '@vercel/postgres';
  
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -20,4 +21,8 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date) VALUES ( ${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 }
